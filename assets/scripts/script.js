aa5964c9d5b2d2512210f73ad51241148230eb00
dmfriendjr <davidmfriendjr@gmail.com>
2017-12-12 19:18:01 -0500

    Added function to take in text input from search form and get response from geocoding api

---
+++ assets/scripts/script.js
@@ -0,0 +1,18 @@
+var googleGeocodeKey = `AIzaSyDg1N8wtIIuCBZNZlqOMB7sVCKTYxMZIpY`;
+
+$('#location-search-submit-btn').on('click', (event) => {
+	//Get input value
+	let inputText = $('location-search-input').val();
+	doGeocodingRequest(inputText);
+});
+
+function doGeocodingRequest(location) {
+	var requestUrl = `https://maps.googleapis.com/maps/api/geocode/json?address=${location}&key=${googleGeocodeKey}`;
+
+	$.ajax({
+		method: 'GET',
+		url: requestUrl
+	}).done((response) => {
+		console.log(response);
+	});
+}
