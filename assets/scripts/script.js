70adf348683ccea33f32180ca499af14bf3b9257
rmccracken <rob.mccrackenjr@gmail.com>
2017-12-15 10:30:57 -0500

    texxt sentiment is now anylizing the tweets

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,29 +1,6 @@
 let popularTweetData = $('#popular-tweets').text();
-let searchResults;
-console.log('popularTweetData', popularTweetData);
 
-let form = new FormData();
-form.append("text", popularTweetData);
 
-let settings = {
-  "async": true,
-  "crossDomain": true,
-  "url": "https://text-sentiment.p.mashape.com/analyze",
-  "method": "POST",
-  "headers": {
-    "x-mashape-key": "JahSDCynJfmsh9D7aDmHnI63qsDYp1047atjsnvuyr2AKu7PPa",
-    "cache-control": "no-cache",
-    "postman-token": "11b32391-b270-ed47-09d3-94474d4f94c4"
-  },
-  "processData": false,
-  "contentType": false,
-  "mimeType": "multipart/form-data",
-  "data": form
-}
-
-$.ajax(settings).done(function (response) {
-  console.log(response);
-});
 
 
 function doTwitterSearch(searchTerm, searchType) {
@@ -62,6 +39,30 @@ function processTweetResults(response) {
 		}
 
 	}
+	console.log(searchResults);
+
+let form = new FormData();
+form.append("text", searchResults);
+
+let settings = {
+  "async": true,
+  "crossDomain": true,
+  "url": "https://text-sentiment.p.mashape.com/analyze",
+  "method": "POST",
+  "headers": {
+    "x-mashape-key": "JahSDCynJfmsh9D7aDmHnI63qsDYp1047atjsnvuyr2AKu7PPa",
+    "cache-control": "no-cache",
+    "postman-token": "11b32391-b270-ed47-09d3-94474d4f94c4"
+  },
+  "processData": false,
+  "contentType": false,
+  "mimeType": "multipart/form-data",
+  "data": form
+}
+
+$.ajax(settings).done(function (response) {
+  console.log(response);
+});
 }
 
 $('#location-search-submit-btn').on('click', (event) => {
