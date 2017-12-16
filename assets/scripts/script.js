97c07185add1fe304b7b5f5b2041b844b7dde55e
dmfriendjr <davidmfriendjr@gmail.com>
2017-12-16 10:56:54 -0500

    Added popular and recent tweets to approriate columns on search

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,14 +1,12 @@
 let popularTweetData = $('#popular-tweets').text();
 
-
-
-
 function doTwitterSearch(searchTerm, searchType) {
 	$.ajax({
 		method: 'GET',
 		url: `https://twitter-trending-analysis.herokuapp.com/tweets/?q=${searchTerm}?t=${searchType}`,
 	}).done( (response) => {
-		processTweetResults(JSON.parse(response));
+		let targetHTML = searchType === 'popular' ? document.getElementById('popular-tweets') : document.getElementById('recent-tweets');
+		processTweetResults(JSON.parse(response),targetHTML);
 	});
 }
 
@@ -22,29 +20,28 @@ function displayTweet(targetHTML, tweetId) {
 	 	});	
 }
 
-function processTweetResults(response) {
+function processTweetResults(response,targetHTML) {
 	this.searchResults = [];
 	let displayTweets = 0;
 	console.log(response);
 	let displayedTweetIds = [''];
 	
+	console.log('Target HTML', targetHTML);
 	for (let i = 0; i < response.statuses.length; i++) {
-
-
 		if (response.statuses[i].hasOwnProperty('retweeted_status')) {
-			if (displayedTweetIds.indexOf(response.statuses[i].retweeted_status.id_str) !== -1) {
-				console.log('List contains id already', displayedTweetIds);
-			} else {
-				this.searchResults.push(response.statuses[i].retweeted_status.full_text);			
-				displayedTweetIds.push(response.statuses[i].retweeted_status.id_str);
-				this.displayTweet(document.getElementById('recent-tweets'),response.statuses[i].retweeted_status.id_str);
+			if (displayedTweetIds.indexOf(response.statuses[i].retweeted_status.id_str) === -1) {
+				this.searchResults.push(response.statuses[i].retweeted_status.full_text);
+				if (displayTweets < 25) {
+					displayedTweetIds.push(response.statuses[i].retweeted_status.id_str);
+					this.displayTweet(targetHTML,response.statuses[i].retweeted_status.id_str);
+				}
 			}
 		} else {
 			this.searchResults.push(response.statuses[i].full_text);
-
+			displayedTweetIds.push(response.statuses[i].id_str);
 			if (displayTweets < 25) {
 				displayTweets++;
-				this.displayTweet(document.getElementById('popular-tweets'),response.statuses[i].id_str);
+				this.displayTweet(targetHTML,response.statuses[i].id_str);
 			}
 		}
 
@@ -75,5 +72,7 @@ $.ajax(settings).done(function (response) {
 }
 
 $('#location-search-submit-btn').on('click', (event) => {
-	doTwitterSearch($('#location-search-input').val(), 'popular');
+	let searchTerm = $('#location-search-input').val();
+	doTwitterSearch(searchTerm, 'popular');
+	doTwitterSearch(searchTerm, 'recent');
 });
