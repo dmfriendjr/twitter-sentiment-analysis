a05598330c7dffae585112e865b2c56382735412
rmccracken <rob.mccrackenjr@gmail.com>
2017-12-16 12:54:40 -0500

    converted sentiment to json object

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,129 +1,120 @@
-let popularTweetData = $('#popular-tweets').text();
+ let popularTweetData = $('#popular-tweets').text();
 
-function doTwitterSearch(searchTerm, searchType) {
-	$.ajax({
-		method: 'GET',
-		url: `https://twitter-trending-analysis.herokuapp.com/tweets/?q=${searchTerm}?t=${searchType}`,
-	}).done( (response) => {
-		let targetHTML = searchType === 'popular' ? document.getElementById('popular-tweets') : document.getElementById('recent-tweets');
-		processTweetResults(JSON.parse(response),targetHTML);
-	});
-}
+ function doTwitterSearch(searchTerm, searchType) {
+     $.ajax({
+         method: 'GET',
+         url: `https://twitter-trending-analysis.herokuapp.com/tweets/?q=${searchTerm}?t=${searchType}`,
+     }).done((response) => {
+         let targetHTML = searchType === 'popular' ? document.getElementById('popular-tweets') : document.getElementById('recent-tweets');
+         processTweetResults(JSON.parse(response), targetHTML);
+     });
+ }
 
-function getTrendingTopics() {
-	$.ajax({
-		method: 'GET',
-		url: `https://twitter-trending-analysis.herokuapp.com/trending/?id=2459115`,
-	}).done( (response) => {
-		displayTrendingTopics(JSON.parse(response));	
-	});
-}
+ function getTrendingTopics() {
+     $.ajax({
+         method: 'GET',
+         url: `https://twitter-trending-analysis.herokuapp.com/trending/?id=2459115`,
+     }).done((response) => {
+         displayTrendingTopics(JSON.parse(response));
+     });
+ }
 
-function displayTrendingTopics(response) {
-	response = response[0].trends;
+ function displayTrendingTopics(response) {
+     response = response[0].trends;
 
-	let targetDiv = $('#trending-topics');
-	targetDiv.empty();
-	for (let i = 0; i < response.length; i++) {
-		let newButton = $('<button>', {
-			'class': 'btn btn-secondary',
-			'data-query': response[i].query,
-			text: response[i].name,
-			click: (event) => {
-				this.doTwitterSearch( $(event.target).text(), 'popular');
-				this.doTwitterSearch( $(event.target).text(), 'recent');				
-			}
-		});
+     let targetDiv = $('#trending-topics');
+     targetDiv.empty();
+     for (let i = 0; i < response.length; i++) {
+         let newButton = $('<button>', {
+             'class': 'btn btn-secondary',
+             'data-query': response[i].query,
+             text: response[i].name,
+             click: (event) => {
+                 this.doTwitterSearch($(event.target).text(), 'popular');
+                 this.doTwitterSearch($(event.target).text(), 'recent');
+             }
+         });
 
-		targetDiv.append(newButton);
-	}
-}
+         targetDiv.append(newButton);
+     }
+ }
 
-function displayTweet(targetHTML, tweetId) {
-	twttr.widgets.createTweet(tweetId,targetHTML,
-		{
-		 align: 'left'
-		})
-	  	.then(function (el) {
-			twttr.widgets.load();
-	 	});	
-}
+ function displayTweet(targetHTML, tweetId) {
+     twttr.widgets.createTweet(tweetId, targetHTML, {
+             align: 'left'
+         })
+         .then(function(el) {
+             twttr.widgets.load();
+         });
+ }
 
-function processTweetResults(response,targetHTML) {
-	this.searchResults = [];
-	let displayTweets = 0;
-	let displayedTweetIds = [''];
-	
-	for (let i = 0; i < response.statuses.length; i++) {
-		if (response.statuses[i].hasOwnProperty('retweeted_status')) {
-			if (displayedTweetIds.indexOf(response.statuses[i].retweeted_status.id_str) === -1) {
-				this.searchResults.push(response.statuses[i].retweeted_status.full_text);
-				if (displayTweets < 25) {
-					displayTweets++;
-					displayedTweetIds.push(response.statuses[i].retweeted_status.id_str);
-					this.displayTweet(targetHTML,response.statuses[i].retweeted_status.id_str);
-				}
+ function processTweetResults(response, targetHTML) {
+     this.searchResults = [];
+     let displayTweets = 0;
+     let displayedTweetIds = [''];
 
-			}
-		} else {
-			this.searchResults.push(response.statuses[i].full_text);
-			displayedTweetIds.push(response.statuses[i].id_str);
-			if (displayTweets < 25) {
-				displayTweets++;
-				this.displayTweet(targetHTML,response.statuses[i].id_str);
-			}
-		}
+     for (let i = 0; i < response.statuses.length; i++) {
+         if (response.statuses[i].hasOwnProperty('retweeted_status')) {
+             if (displayedTweetIds.indexOf(response.statuses[i].retweeted_status.id_str) === -1) {
+                 this.searchResults.push(response.statuses[i].retweeted_status.full_text);
+                 if (displayTweets < 25) {
+                     displayTweets++;
+                     displayedTweetIds.push(response.statuses[i].retweeted_status.id_str);
+                     this.displayTweet(targetHTML, response.statuses[i].retweeted_status.id_str);
+                 }
 
-	}
+             }
+         } else {
+             this.searchResults.push(response.statuses[i].full_text);
+             displayedTweetIds.push(response.statuses[i].id_str);
+             if (displayTweets < 25) {
+                 displayTweets++;
+                 this.displayTweet(targetHTML, response.statuses[i].id_str);
+             }
+         }
 
-	this.doSentimentAnalysis(searchResults);
-}
+     }
 
-<<<<<<< HEAD
-$.ajax(settings).done(function (response) {
-	data(JSON.parse(response));
-  console.log(response);
-=======
+     this.doSentimentAnalysis(searchResults);
+ }
 
+ $(document).ready(() => {
+     this.getTrendingTopics();
+ });
 
-$(document).ready(() => {
-	this.getTrendingTopics();	
->>>>>>> a9ffd5e8735df65742a3cf82c7fd9f8c36319014
-});
+ function doSentimentAnalysis(searchResults) {
+     for (let i = 0; i < searchResults.length; i++) {
+         let form = new FormData();
+         form.append("text", searchResults[i]);
 
-function doSentimentAnalysis(searchResults)
-{
-	for (let i = 0; i < searchResults.length; i++) {
-		let form = new FormData();
-		form.append("text", searchResults[i]);
+         let settings = {
+             "async": true,
+             "crossDomain": true,
+             "url": "https://text-sentiment.p.mashape.com/analyze",
+             "method": "POST",
+             "headers": {
+                 "x-mashape-key": "JahSDCynJfmsh9D7aDmHnI63qsDYp1047atjsnvuyr2AKu7PPa",
+                 "cache-control": "no-cache",
+                 "postman-token": "11b32391-b270-ed47-09d3-94474d4f94c4"
+             },
+             "processData": false,
+             "contentType": false,
+             "mimeType": "multipart/form-data",
+             "data": form
+         }
 
-		let settings = {
-		  "async": true,
-		  "crossDomain": true,
-		  "url": "https://text-sentiment.p.mashape.com/analyze",
-		  "method": "POST",
-		  "headers": {
-		    "x-mashape-key": "JahSDCynJfmsh9D7aDmHnI63qsDYp1047atjsnvuyr2AKu7PPa",
-		    "cache-control": "no-cache",
-		    "postman-token": "11b32391-b270-ed47-09d3-94474d4f94c4"
-		  },
-		  "processData": false,
-		  "contentType": false,
-		  "mimeType": "multipart/form-data",
-		  "data": form
-		}
+         $.ajax(settings).done(function(response) {
+         	  let sentimentObject = (JSON.parse(response));
+            console.log(sentimentObject);
+         });
 
-		$.ajax(settings).done(function (response) {
-		  //console.log(response);
-		});
-	
-	}	
-}
+     }
+ }
 
 
-$('#location-search-submit-btn').on('click', (event) => {
-	event.preventDefault();
-	let searchTerm = $('#location-search-input').val();
-	doTwitterSearch(searchTerm, 'popular');
-	doTwitterSearch(searchTerm, 'recent');
-});
+ $('#location-search-submit-btn').on('click', (event) => {
+     event.preventDefault();
+     let searchTerm = $('#location-search-input').val();
+     doTwitterSearch(searchTerm, 'popular');
+     doTwitterSearch(searchTerm, 'recent');
+ });
